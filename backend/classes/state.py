bf3991e00cc803eefd9822dbf6d39b05b75df353
jonfodi <masterfodi@gmail.com>
2025-08-24 20:59:16 -0400

    make state more flexible (no rigid shoe model)

--- backend/classes/state.py
+++ backend/classes/state.py
@@ -4,14 +4,7 @@ from typing import TypedDict, NotRequired, Required, Dict, List, Any
 # Define a single shoe item
 # ideally this would be data that a nike api would need to retrieve an item and process a payment
 class ShoeItem(TypedDict):
-    shoe_type: str
-    price: float 
-    size: str     
-    color: str
-    url: NotRequired[str]
-    availability: NotRequired[str]
-    description: NotRequired[str]
-    id: NotRequired[str] # nike would give this. use for payment 
+    pass
 
 # make gender enum of either male or female
 class Gender(Enum):
@@ -25,6 +18,7 @@ class ShoeType(Enum):
 
 
 # Define the input state
+# NOT USING SHOE TYPE OR SIZE RIGHT NOW IN PYDANTIC MODEL RIGHT NOW 
 class InputState(TypedDict, total=False):
     shoe_type: Required[str]
     size: Required[float]
@@ -34,6 +28,7 @@ class InputState(TypedDict, total=False):
 
 class ShoppingState(InputState):
     url: str
-    shoes: List[ShoeItem]  # List of all extracted shoes
-    extraction_status: NotRequired[str]  # Track extraction success/failure
     shoe_data: Dict[str, Any]
+    shoes: Dict[str, Any]  # List of all extracted shoes
+    extraction_status: NotRequired[str]  # Track extraction success/failure
+
